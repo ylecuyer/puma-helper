b2085c3f5a768cf0558b58ab2815a4a0eb759fdc
Valentin Pichard <valentin.pichard@dimelo.com>
2019-01-04 13:35:10 +0100

    Fix relative path exec binary
    Signed-off-by: Valentin Pichard <valentin.pichard@dimelo.com>

--- config/config.go
+++ config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import v "github.com/spf13/viper"
+import (
+	"os"
+	"path/filepath"
+
+	v "github.com/spf13/viper"
+)
 
 // InitConfig load config from file and/or the environment.
 func InitConfig() error {
@@ -8,6 +13,12 @@ func InitConfig() error {
 	v.SetConfigType("yaml")
 	v.AddConfigPath("$HOME")
 	v.AddConfigPath(".")
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return err
+	}
+	v.AddConfigPath(dir)
 
 	return v.ReadInConfig()
 }
